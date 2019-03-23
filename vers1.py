847c02d32f1943d7a3877d983902abfa0798a5cc
CottonVats <47786883+CottonVats@users.noreply.github.com>
2019-03-23 23:00:09 +0300

    Add files via upload

--- vers1.py
+++ vers1.py
@@ -1,5 +1,7 @@
 from urllib.parse import urlencode
 import requests
+import json
+import time
 oauth_url = 'htpps://oauth.vk.com/authorize'
 app_id = 6896760
 auth_params = {
@@ -10,7 +12,7 @@ auth_params = {
     'v': 5.92
 }
 print("?".join((oauth_url, urlencode(auth_params))))
-token1 = "5e354313b78f73ef38950d096b4b462bdcd2847acfac2c4c8bd244c0d6609e48469871f1409b32bd14d6f"
+token1 = "6cce3f3c00860bbd32b8f8271cd083184c0c092dafcd6f35d2edc26a3ffec55396301ba6f7a7999631427"
 class VK_user:
     def __init__(self, token):
         self.token = token
@@ -27,6 +29,7 @@ class VK_user:
         self.groups_set = set(self.groups.json()['response']['items'])
         self.params_list = []
         self.sets_list = []
+        self.all_group_ids_list = []
         for id in self.friends_ids:
             friends_params = {
                 "access_token":self.token,
@@ -36,13 +39,15 @@ class VK_user:
             self.params_list.append(friends_params)
         for friend_params in self.params_list:
             response = requests.get('https://api.vk.com/method/groups.get', friend_params)
+            time.sleep(1)
             response_text = response.json()['response']
-            groups_ids_set = set(response_text['items'])
-            self.sets_list.append(groups_ids_set)
-
-
-
-
+            groups_ids = response_text['items']
+            for id in groups_ids:
+                self.all_group_ids_list.append(id)
+        self.friends_group_ids_set = set(self.all_group_ids_list)
+        self.nonmultual_groups = self.groups_set.difference_update(self.friends_group_ids_set)
+        with open('groups.json', 'w') as file:
+            json.dump(self.nonmultual_groups, file)
 
 
 im = VK_user(token1)
